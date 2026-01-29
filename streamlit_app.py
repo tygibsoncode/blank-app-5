1acc1dd114037b2256c47335ed6db9a098c3790d
tygibsoncode <tymichaelgibson@gmail.com>
2026-01-29 14:37:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import time
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Simple Dashboard")
+
+basic_plots = st.checkbox("Basic Plots")
+if basic_plots:
+    table = pd.DataFrame(
+        np.random.randn(20,3),
+                         columns=["A","B","C"]
+    )
+    st.dataframe(table)
+    st.line_chart(table)
