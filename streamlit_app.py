8368233d0450a324ca9dfc9b4e307b77c27b3dca
tygibsoncode <tymichaelgibson@gmail.com>
2026-01-29 14:47:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,30 +8,36 @@ st.title("Simple Dashboard")
 basic_plots = st.checkbox("Basic Plots")
 if basic_plots:
     table = pd.DataFrame(
-        np.random.randn(20,3),
-                         columns=["A","B","C"]
+        np.random.randn(20, 3),
+        columns=["A", "B", "C"]
     )
     st.dataframe(table)
     st.line_chart(table)
 
 sliders = st.checkbox("Sliders")
 if sliders:
-    age = st.slider("How old are you?",18,120,20)
-    someRange = st.slider("Select a range",
-                          0.0, 200.0, (25., 50))
-    dentistAppointment = st.slider("Select a time",
-                                   value=(time(10,30),
-                                   time(12,30)))
-    
+    age = st.slider("How old are you?", 18, 120, 20)
+
+    someRange = st.slider(
+        "Select a range",
+        min_value=0.0,
+        max_value=200.0,
+        value=(25.0, 50.0)
+    )
+
+    dentistAppointment = st.slider(
+        "Select a time",
+        value=(time(10, 30), time(12, 30))
+    )
+
 st.divider()
 
-tab1,tab2,tab3 = st.tabs(["About Us","Mission","Contact Us"])
+tab1, tab2, tab3 = st.tabs(["About Us", "Mission", "Contact Us"])
 with tab1:
     st.write("We are CAP 4104 students at FIU.")
-    
+
 with tab2:
     st.write("Our mission is to use HCI principles effectively.")
-    
+
 with tab3:
     st.write("Office hours are Wednesday in INV1 455 at 2pm.")
-    
